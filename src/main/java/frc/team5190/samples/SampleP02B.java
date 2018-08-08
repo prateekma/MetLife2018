9663e82c35b022f96f1a6166284bd9d9bb2aa8b9
Prateek Machiraju <prateek.machiraju@gmail.com>
2018-08-07 23:40:12 -0400

    Added Missing Files

---
+++ src/main/java/frc/team5190/samples/SampleP02B.java
@@ -0,0 +1,21 @@
+package frc.team5190.samples;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class SampleP02B extends TimedRobot {
+
+    private Long timer = 0L;
+
+    @Override
+    public void autonomousInit() {
+        timer = System.currentTimeMillis();
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+        if (timer > 20000) {
+            System.out.println("Hello, world!");
+            timer = 0L;
+        }
+    }
+}
